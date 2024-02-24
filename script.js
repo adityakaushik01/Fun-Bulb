4dface45ef401a3989cc5578b3bdc4e2744f1be6
Aditya Kaushik <109030417+adityakaushik01@users.noreply.github.com>
2024-02-25 02:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 let onbulb = document.getElementById('on');
 let offbulb = document.getElementById('off');
-let main = document.getElementById('main');
+let onbtn = document.getElementById('onbtn');
+let offbtn = document.getElementById('offbtn');
 
 
-onbulb.addEventListener('click', ()=>{
-    onbulb.style.display = 'none';
-    offbulb.style.display = 'block';
-})
-
-offbulb.addEventListener('click', ()=>{
-    offbulb.style.display = 'none';
-    onbulb.style.display = 'block';
-})
-
-// main.addEventListener('click', ()=>{
-//     main.style.backgroundColor = 'red';
+// onbulb.addEventListener('click', ()=>{
+//     onbulb.style.display = 'none';
+//     offbulb.style.display = 'block';
 // })
 
+// offbulb.addEventListener('click', ()=>{
+//     offbulb.style.display = 'none';
+//     onbulb.style.display = 'block';
+// })
+
+onbulb.addEventListener('click', on);
+
+offbulb.addEventListener('click', off);
+
+onbtn.addEventListener('click', off);
+
+offbtn.addEventListener('click', on);
+
+
+
+function off() {
+    offbulb.style.display = 'none';
+    onbulb.style.display = 'block';
+}
+
+function on() {
+    onbulb.style.display = 'none';
+    offbulb.style.display = 'block';
+}
